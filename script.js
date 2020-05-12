c6364a22785d827eba4475724e4ee6956d8fe2b7
KamranJabiyev <kamran.jabiyev@code.edu.com>
2020-05-12 20:48:48 +0400

    comit

---
+++ script.js
@@ -0,0 +1,30 @@
+// var a=10;
+
+// // a+=10; //a=a+10
+
+// // a-=5; //a=20-5
+
+// // a%=3; //a=a%3
+
+// a**=3; //a=10**2
+
+// var a=10
+// var b=10
+
+// console.log(!((a>=b || a<10) && a==10))
+
+var age=19
+
+// if(age>16)
+// {
+//    console.log("Yashiniz uygundur") 
+// }
+// else
+// {
+//     console.log("Yashiniz chatmir") 
+// }
+
+var result=(age>16) ? "Yashiniz uygundur":"Yashiniz chatmir"
+
+console.log(result)
+
